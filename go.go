f4b654565a6320a13004c0792fa8bc37d9394ebe
davv12 <142831628+davv12@users.noreply.github.com>
2023-08-22 17:37:41 -0300

    Add files via upload

---
+++ go.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+func main() {
+var numero int
+fmt.Println("informe um numero")
+fmt.Scan(&numero)
+var numero1 int
+var numero2 int
+var numero3 int
+
+fun	numero1 = numero * 2
+	numero2 = numero * 3
+	numero3 = numero * 4
+	fmt.Println("o dobro é", numero1, "o triplo é", numero2, "o quadruplo é ", numero3)
+}
