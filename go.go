f1946e2b43c772d55b52379285c748540aed1acf
davv12 <142831628+davv12@users.noreply.github.com>
2023-08-22 17:19:10 -0300

    Add files via upload

---
+++ go.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var numero1 int
+	fmt.Println("fale o primeiro numero")
+	fmt.Scan(&numero1)
+
+	var numero2 int
+	fmt.Println("fale o segundo numero")
+	fmt.Scan(&numero2)
+
+	var numero3 int
+
+	fmt.Println("fale o terceiro numero")
+	fmt.Scan(&numero3)
+
+	var soma = numero1 + numero2 + numero3
+	fmt.Println("a soma dos numeros é", soma)
+
+}
